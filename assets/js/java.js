a125444156a870e3fa844cfd90eb491b72eaca08
Shelisha Rutherford <starshipazul@yahoo.com>
2019-06-13 21:29:07 -0400

    FINISHED!

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,7 +1,7 @@
 //creat an array of ships
 
-var ships = ['Tardis', 'Enterprise', 'Battlestar', 'Death Star', 'Elysium ship',
-    'Millennium', 'Serenity', 'Event Horizon', 'Sulaco'];
+var ships = ['tardis', 'enterprise', 'battlestar', 'deathstar', 'elysium',
+    'millennium', 'serenity', 'eventhorizon', 'sulaco'];
 
 
 
@@ -14,7 +14,7 @@ var underscores = 0;
 var underSnCletters = [];
 // Wrong guesses
 var wrong = [];
-
+var searchLetter = [];
 // Game counters
 var wins = 0;
 var losses = 0;
@@ -31,6 +31,12 @@ function Start() {
     letters = chooseShip.split("");
     underscores = letters.length;
     wrong = [];
+    forWins();
+    forLosses();
+    forTries();
+
+
+
     for (var i = 0; i < underscores; i++) {
         underSnCletters.push("_");
     }
@@ -39,21 +45,22 @@ function Start() {
     console.log(chooseShip);
     console.log(letters);
     console.log(underscores);
+   
     // set up code to link to html
     document.getElementById("under-scores").innerHTML = underSnCletters.join(" ");
-    document.getElementById("remaining-guess").innerHTML = numTries;
-    document.getElementById("num-wins").innerHTML = wins;
-    document.getElementById("num-losses").innerHTML = losses;
+    //document.getElementById("remaining-guess").innerHTML = numTries;
+    // document.getElementById("num-wins").innerHTML = wins;
+    // document.getElementById("num-losses").innerHTML = losses;
 
-    // forTries(); 
-    // completeOneGame();
-    // forIncomplete();
-    // forWrong();
 
 }
+//======= function to find the right letter====
+Start();
+
 
 function testKey(letters) {
 
+    //alert(letters);
     var searchLetter = false;
 
     for (var i = 0; i < underscores; i++) {
@@ -76,6 +83,7 @@ function testKey(letters) {
         // }
 
     }
+    //====== this will push the letters to the wrong guesses
     else {
         wrong.push(letters);
         numTries--
@@ -86,38 +94,40 @@ function testKey(letters) {
 }
 
 function gameOver() {
-    console.log("numWins: " + wins + " numLoss: " + losses + "  NumGuesses: " + numTries);
-  
-    document.getElementById(" ").innerHTML = ;
+    console.log(`Wins: ${wins}, Losses: ${losses}, Tries: ${numTries}`);
+   
 
-    document.getElementById(" ").innerHTML = losses;
-    document.getElementById(" ").innerHTML = losses;
-    
-    
-    if (searchLetter.toString() == underSnCletters.toString()) {
+    // document.getElementById("remaining-guess").innerHTML = numTries;
+
+    var usedkeys = underSnCletters.join(" ");
+    var wrongKeys = wrong.join(" ");
+    document.getElementById("under-scores").innerHTML = usedkeys;
+    document.getElementById("wrong-guess").innerHTML = wrongKeys;
+    forTries();
+   
+    // underscorces and correct letters should populate here. These should match and restart the game
+    if (letters.toString() === underSnCletters.toString()) {
         wins++;
         alert("Winning");
-
-        document.getElementById("num-wins").innerHTML = wins;
-
+        forWins();
         Start();
 
     }
 
-
+    // needs to tell the user that they have lost the game
     else if (numTries === 0) {
         losses++;
-        alert("Looser");
-
-        document.getElementById("num-losses").innerHTML = losses;
-        start();
+        alert("Oh snap! Try again!");
+      
+        forLosses();
+        Start();
     }
 }
 
 
 
 
-Start();
+//Start();
 
 
 //============================SET UP FUNCTION FOR THE KEYS TO REGISTER===============
@@ -126,11 +136,15 @@ Start();
 document.onkeyup = function (event) {
     var keypress = String.fromCharCode(event.keyCode).toLowerCase();
     testKey(keypress);
+    gameOver();
     console.log(keypress);
 
 }
 
 
+function forWins() { document.getElementById("num-wins").innerHTML = wins; }
+function forLosses() { document.getElementById("num-losses").innerHTML = losses; }
+function forTries() { document.getElementById("remaining-guess").innerHTML = numTries; }
 
 
 
@@ -142,90 +156,4 @@ document.onkeyup = function (event) {
 
 
 
-
-
-
-
-
-// Initiate click listners
-// document.onkeyup = function (event) {
-
-//     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-
-//     matchLetters(letterGuessed);
-
-//     // completeOneGame();
-// }
-
-// function matchLetters(matchLet) {
-//     // a boolean to see if the right letter is used
-//     var foundLetters = false;
-
-//     // checkin gor the corret letter 
-//     for (var i = 0; i < underscores; i++) {
-//         if (chooseShip[i] === matchLet) {
-
-//             foundLetters = true;
-//         }
-//     }
-
-//     // This will put the letter where is belongs 
-//     if (foundLetters) {
-//         // Loop through the word
-//         for (var l = 0; l < underscores; l++) {
-//             //  under scores and correct letters collected
-//             if (chooseShip[l] === matchLet) {
-//                 // Set the specific space in blanks and letter equal to the letter when there is a match
-//                 underSnCletters[l] = matchLet;
-//             }
-//         }
-//         // Test
-//         console.log(underSnCletters);
-//     }
-//     // wrong letters not in the word
-//     else {
-//         // pushes the wrong letters to a list on wrong letters.
-//         wrong.push(matchLet);
-//         numTries--;
-//     }
-
-// }
-
-
-// function completeOneGame() {
-//     // status updates
-//     console.log("numWins: " + wins + " numLoss: " + losses + "  NumGuesses: " + numTries);
-
-
-//     // If all the letters match the solution
-//     if (letters.toString() === underSnCletters.toString()) {
-//         //  alert the user
-//         wins++;
-//         alert("You win!");
-
-//         //   restart the game and update the win counter
-//         document.getElementById("num-wins").innerHTML = wins;
-//         startGame();
-//     }
-
-//     // If user runs out of guesses
-//     else if (numTries === 0) {
-//         // Add to the loss counter 
-//         losses++;
-
-
-//         //  restart the game and update losses
-//         document.getElementById("num-loss").innerHTML = losses;
-//         startGame();
-//     }
-// };
-
-
-
-
-// function forTries() { document.getElementById("num-tries").innerHTML = numTries; }
-// // Print  guesses and blanks in the webpage
-// function forIncomplete() { document.getElementById("incomplete-ans").innerHTML = underSnCletters.join(" "); }
-// // Print wrong guesses in the webpage
-// function forWrong() { document.getElementById("wrong-letter").innerHTML = wrong.join(" "); }
 
